512c4f6bceae0b94533f8e3fb577d8e8f96379f5
Rdoolz51 <Rdoolz51@aol.com>
2022-06-27 08:23:42 -0400

    Create nameGenerator.js

---
+++ nameGenerator.js
@@ -0,0 +1,19 @@
+//fill out each array with fantasy names
+var firstName = ["Ryan", "Dan", "Alfred", "Tod", "Asher", "Carter", "Turner", "Piper"];
+var lastName = ["Savage", "Alonso", "Remmington", "Glide","Atwood", "Bellsmith", "Clarkwright"];
+
+//change $("#check") to whatever div you want to attach the button to.
+buttonEl = $('<button>')
+buttonEl.text("GENERATE NAME")
+$("#check").append(buttonEl)
+buttonEl.on('click', nameGen)
+
+
+function nameGen() {
+    var rFirst = Math.floor(Math.random() * firstName.length);
+    var rLast = Math.floor(Math.random() * lastName.length);
+    
+    var randomName = firstName[rFirst] + " " + lastName[rLast];
+    
+    return randomName;
+}
