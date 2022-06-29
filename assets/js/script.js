eacde8ef8b6aa0f4b58c084b6baed5b09d2e93cf
Ryan Dooley <Rdoolz51@aol.com>
2022-06-29 13:00:31 -0400

    added instructions button to display instruction card once completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,16 +9,17 @@ $("#statCard").on('click', statCreate)
 $("#equipCard").on('click', equipCreate)
 $("#spellCard").on('click', spellCreate)
 $("#vcs").on('click', charSheet)
+$("#ins").on('click', instructions)
 
 $('#burger').on('click', hamburgerMenu)
-   
+
 
 function hamburgerMenu() {
     if ($('.c-pan').hasClass("zero")) {
         $('.c-pan').css("transform", "translateX(1000px)")
         $('.c-pan').removeClass("zero")
-        
-    } 
+
+    }
     else {
         $('.c-pan').css("transform", "translateX(0)")
         $('.c-pan').addClass("zero")
@@ -26,24 +27,44 @@ function hamburgerMenu() {
     }
 }
 
-function charCreate () {
+function charCreate() {
 }
-function statCreate () {
+function statCreate() {
 
 }
-function equipCreate () {
+function equipCreate() {
 
 }
-function spellCreate () {
+function spellCreate() {
 
 }
 
 function charSheet() {
     if ($('.main-card').css("display") == "flex") {
+        if ($('.main-card').css("background-image", "url(/assets/images/instructions.png)")) {
+            $('.main-card').css("background-image", "url(/assets/images/charsheet.png)");
+        }
+        else{
+        }
         $('.main-card').css("display", "none")
-        
     }
     else {
         $('.main-card').css("display", "flex")
+        $('.main-card').css("background-image", "url(/assets/images/charsheet.png)");
+    }
+}
+
+function instructions() {
+    if ($('.main-card').css("display") == "flex") {
+        if ($('.main-card').css("background-image", "url(/assets/images/charsheet.png)")) {
+            $('.main-card').css("background-image", "url(/assets/images/instructions.png)");
+        }
+        else{
+        }
+        $('.main-card').css("display", "none")
+    }
+    else {
+        $('.main-card').css("display", "flex")
+        $('.main-card').css("background-image", "url(/assets/images/instructions.png)");
     }
 }
