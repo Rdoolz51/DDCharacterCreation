c7ec17bb754aebd4c3834f3b68ce58d57ebd642e
Ryan Dooley <Rdoolz51@aol.com>
2022-06-29 21:45:24 -0400

    merged template work

--- assets/js/template.js
+++ assets/js/template.js
@@ -1,3 +1,9 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.modal').modal();
-  });
+});
+$("#demo").on("click", function() {
+    if ($("#demo").prop('checked') == true){
+        console.log("on")
+    } else {
+        console.log("off")
+    }})
