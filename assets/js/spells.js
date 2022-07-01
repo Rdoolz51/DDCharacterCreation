99d12ffb5647c64c963a3f7a9878b92e5f0df7fc
Buckimus <ryan42buck@gmail.com>
2022-07-01 19:58:11 -0400

    added some comments and made way to identify dynamic checkboxes

--- assets/js/spells.js
+++ assets/js/spells.js
@@ -17,12 +17,11 @@ var CharacterAttributes = {
   equipment: [],
   spells: [],
 };
-// var cantripsKnown = "";
 var submitSpells = $("#submit-spells");
 var selectedSpells = ["15", "12", "25"];
 var baseApiUrl = "https://www.dnd5eapi.co/api/";
 
-// GOING TO HAVE TO CHANGE ONCE WE GET CLASSES SET UP FROM OTHER PAGES
+// GOING TO HAVE TO CHANGE ONCE WE GET CLASSES SET UP FROM OTHER PAGES!!!!!!!!!!!!!
 
 var classUrl = `classes/druid/levels/1/spells`;
 
@@ -32,6 +31,9 @@ $(document).ready(function () {
 // $(document).ready(function () {
 //   $(".tooltipped").tooltip();
 // });
+// Event listener on the first submit button shown.
+// Hides said submit button and switch.
+// creates new submit button at bottom of page
 
 $("#submit-choice").on("click", function () {
   console.log("clicked");
@@ -51,6 +53,7 @@ $("#submit-choice").on("click", function () {
 });
 // })
 
+// fetches spells that the user selected/randomized has available
 var classSpells = function (event) {
   var requestUrl = baseApiUrl + classUrl;
   fetch(requestUrl)
@@ -69,11 +72,13 @@ var classSpells = function (event) {
     });
 };
 
+// displays spells using data from classSpells
 var renderClasses = function (data, index) {
-  var spellsCrd = `<div class="row"><h5 class="col 12" id="spellCar${index}">${data}</h5><label><input type="checkbox" class="red darken-4s" id="box${index}" /><span>Check to use this spell</span></label></div>`;
+  var spellsCrd = `<div class="row"><h5 class="col 12" id="spellCar${index}">${data}</h5><label><input type="checkbox" class=" spell-box red darken-4s"  data-spell="${data}" id="box${index}" /><span>Check to use this spell</span></label></div>`;
   $("#spellsContainer").append(spellsCrd);
 };
 
+// fetches spell description
 var spellsDes = function (data, index) {
   var spellUrl = `/spells/${data}`;
   spellApiUrl = baseApiUrl + spellUrl;
@@ -90,21 +95,13 @@ var spellsDes = function (data, index) {
     });
 };
 
+// displays spell description using data from spellDes
 var renderSpellDescription = function (data, index) {
-  var spellsDescription = `<blockquote>${data}</blockquote>`;
+  var spellsDescription = `<p id="spellDesc"><blockquote>${data}</blockquote></p>`;
   $("#spellCar" + index).append(spellsDescription);
 };
-var setUpSubmit = function () {
-  submitSpells.addClass("tooltipped");
-  submitSpells.attr("data-position", "right");
-  submitSpells.attr(
-    "data-tooltip",
-    `You need to have ${cantripsKnown} spells selected.`
-  );
-  submitSpells.tooltip();
 
-  // return;
-};
+// Fetches amount of cantrips the class chosen has available
 var cantripRestriction = function () {
   var cantripUrl = "classes/druid/levels";
   cantripApiUrl = baseApiUrl + cantripUrl;
@@ -113,13 +110,25 @@ var cantripRestriction = function () {
       return response2.json();
     })
     .then(function (result2) {
-      console.log(result2[1].spellcasting.cantrips_known);
+      //   console.log(result2[1].spellcasting.cantrips_known);
       cantripsKnown = result2[1].spellcasting.cantrips_known;
-      //   cantripsKnown = cantripsKnow;
     })
     .catch(function (err) {
       console.log(err);
     });
+};
+
+// Makes submit button display a tooltip telling user that they have too many spells selected
+var setUpSubmit = function () {
+  submitSpells.addClass("tooltipped");
+  submitSpells.attr("data-position", "right");
+  submitSpells.attr(
+    "data-tooltip",
+    `Your class lets you have no more than ${cantripsKnown} spells selected.`
+  );
+  submitSpells.tooltip();
+
+  // return;
 };
 
 cantripRestriction();
