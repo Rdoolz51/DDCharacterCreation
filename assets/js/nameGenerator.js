fe5d9bece0c2f1f0890b3ae6dc6db132aac5cd35
Ryan Dooley <Rdoolz51@aol.com>
2022-07-02 16:21:13 -0400

    combined both first name arrays into a gender neutral first name option. Got the name generator working. Just add the names.js and nameGenerator.js files to index.html with the names generator being on top

--- assets/js/nameGenerator.js
+++ assets/js/nameGenerator.js
@@ -1,26 +1,21 @@
+nameGen(gender);
 
-//change $("#check") to whatever div you want to attach the button to.
-buttonEl = $('<button>')
-buttonEl.text("GENERATE NAME")
-$("{REPLACE WITH WHAT YOU WANT TO APPEND TO}").append(buttonEl)
-buttonEl.on('click', nameGen)
-
-
-function nameGen() {
-    var rFirst = Math.floor(Math.random() * firstName.length);
+function nameGen(gender) {
+    var randomName = "";
+    var rMFirst = Math.floor(Math.random() * maleFirstName.length);
+    var rFFirst = Math.floor(Math.random() * femaleFirstName.length);
     var rLast = Math.floor(Math.random() * lastName.length);
-    var randomName = firstName[rFirst] + " " + lastName[rLast];
-    $('.randomName').remove();
-    textEl = $('<p>')
-    textEl.addClass("randomName")
-    labelEl = $('<label>')
-    labelEl.addClass("randomName")
-    labelEl.text("Random Name: ")
-    textEl.text(randomName)
-    $('.content').append(labelEl)
-    $(labelEl).append(textEl)
-    // console.log(randomName);
-    
+    var rEither = Math.floor(Math.random() * eitherFirstName[0].length);
+
+    if(gender == 'Male') {
+        randomName = maleFirstName[rMFirst] + " " + lastName[rLast];
+    } else if (gender == 'Female') {
+        randomName = femaleFirstName[rFFirst] + " " + lastName[rLast];
+    } else {
+        randomName = eitherFirstName[0][rEither] + " " + lastName[rLast];
+    }
     return randomName;
 }
+
+console.log(nameGen(gender))
 
