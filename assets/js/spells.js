4ffa009124bc1969498a17d9d8104d1216e100df
Buckimus <ryan42buck@gmail.com>
2022-07-05 13:13:32 -0400

    added youtube api to search class information for the user to see

--- assets/js/spells.js
+++ assets/js/spells.js
@@ -222,7 +222,7 @@ $("#submitChar").on("click", function () {
 
   localStorage.setItem("spells", JSON.stringify(selectedSpells));
 });
-// return to index html
+// return to index
 $("#returnHome").on("click", function () {
   location.href = "./index.html";
 });
---
+++ assets/js/tutorial.js
@@ -0,0 +1,46 @@
+var CharacterAttributes = {
+  name: "",
+  class: "",
+  race: "",
+  alignment: "",
+  sex: "",
+  age: "",
+  height: "",
+  weight: "",
+  proficiencies: "",
+  strength: "",
+  dexterity: "",
+  constitution: "",
+  intelligence: "",
+  wisdom: "",
+  charisma: "",
+  equipment: [],
+  spells: [],
+};
+
+localStorage.getItem();
+var apiKey = "AIzaSyD_iuxaxY56u4gH6ja49Z5q2ZuAcofE7rM";
+var baseApiUrl = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&part=snippet&type=video&q=handbook%20helper%205e%20${CharacterAttributes.class}&maxResults=1`;
+var videoKey;
+var ClassVideo = function () {
+  $.ajax({
+    method: "GET",
+    url: baseApiUrl,
+    success: function (data) {
+      //   console.log(data.items[0].id.videoId);
+      videoKey = data.items[0].id.videoId;
+      var videoplayer = `<iframe
+width="560"
+height="315"
+src="https://www.youtube.com/embed/${videoKey}"
+title="YouTube video player"
+frameborder="0"
+allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+allowfullscreen
+></iframe>`;
+      $("#video").append(videoplayer);
+    },
+  });
+};
+
+ClassVideo();
