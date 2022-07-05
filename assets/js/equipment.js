4905c972b181d75379982e2dc7e4bca115e20d6f
Ryan Dooley <Rdoolz51@aol.com>
2022-07-05 19:38:41 -0400

    working on equipment layout

--- assets/js/equipment.js
+++ assets/js/equipment.js
@@ -7,7 +7,7 @@ var CharacterAttributes = {
     age: '',
     height: '',
     weight: '',
-    proficiencies: '',
+    proficiencies: [],
     strength: '',
     dexterity: '',
     constitution: '',
@@ -17,7 +17,18 @@ var CharacterAttributes = {
     equipment: [],
     spells: [],
 };
-getClassEquipmentApi('paladin');
+const equipBox = document.getElementById("equipmentBox")
+const equipUl = document.createElement("ul");
+const liEl = document.createElement("li");
+const pEl = document.createElement("p");
+
+
+
+function selectEquipment(listLength) {
+    return Math.floor(Math.random() * listLength);
+    
+}
+getClassEquipmentApi('barbarian');
 
 $(document).ready(function () {
     $('.modal').modal();
@@ -25,33 +36,66 @@ $(document).ready(function () {
 function getClassEquipmentApi(playerClass) {
     var apiURL = 'https://www.dnd5eapi.co/api/classes/' + playerClass + '/';
     fetch(apiURL)
-        .then(function (response) {
-            if (response.ok) {
-                return response.json();
-            } else {
-                return Promise.reject(response);
-            }
-        })
-        .then(function (data) {
-            for (let i = 0; i < data.starting_equipment_options.length; i++) {
-                if (data.starting_equipment_options[i].from != undefined) {
-                    for (let j = 0; j < data.starting_equipment_options[i].from.length; j++) {
-                        console.log(data.starting_equipment_options[i]);
-                        if (
-                            data.starting_equipment_options[i].from[j].equipment != undefined ||
-                            data.starting_equipment_options[i].from[j].equipment_category
-                        ) {
-                            //end of line for some equipment
-                        } else {
-                            // console.log(data.starting_equipment_options[i].from[j]);
-                        }
+    .then(function (response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return Promise.reject(response);
+        }
+    })
+    .then(function (data) {
+        if (playerClass == 'paladin') {
+            for (let i = 0; i < data.starting_equipment.length; i++) {
+                const link = data.starting_equipment[i];
+                // Guaranteed Starting
+                console.log(data.starting_equipment[i].equipment.name) //chain mail
+                console.log(data.starting_equipment_options[3].from[0].equipment_category.name); //holy symbols
+                //Equipment
+                console.log("");
+                console.log(" CHOICES : ");
+                console.log("");
+                for (let j = 0; j < data.starting_equipment_options[i].from.length; j++) {
+                    console.log('[1 ' + data.starting_equipment_options[i].from[0][0].equipment.name + " AND " + "1 " + data.starting_equipment_options[i].from[j][1].equipment_option.from.equipment_category.name + "]"); //shield
+                    console.log("OR");
+                        console.log("[2 " + data.starting_equipment_options[i].from[1].equipment_option.from.equipment_category.name + "]");
+                        console.log(" ");
+                        console.log("5 " + data.starting_equipment_options[1].from[0].equipment.name);
+                        console.log("OR");
+                        console.log("1 " + data.starting_equipment_options[1].from[1].equipment_option.from.equipment_category.name);
+                        console.log("");
+                        console.log("1 " + data.starting_equipment_options[2].from[0].equipment.name);
+                        console.log("OR");
+                        console.log("1 " + data.starting_equipment_options[2].from[1].equipment.name);
+                        
                     }
                 }
             }
+            if (playerClass == 'barbarian') {
+                for (let i = 0; i < data.starting_equipment.length; i++) {
+                    const link = data.starting_equipment[i];
+                    // Guaranteed Starting
+                    var num = link.quantity;
+                    console.log(num + " " + link.equipment.name);
+                    //Equipment
+                }
+                console.log("");
+                console.log(" CHOICES : ");
+                console.log("");
+
+                for(let i = 0; i < data.starting_equipment_options.length; i++) {
+                    const link = data.starting_equipment_options[i];
+                    var num = link.from[0].quantity
+                        console.log(num + " " + link.from[0].equipment.name);
+                        console.log("OR");
+                        console.log(num + " " + link.from[1].equipment_option.from.equipment_category.name);
+                        console.log(" ");
+                }
+            }
+
         })
         .catch(function (error) {
             console.log(error);
         });
 }
 
-function getEquipmentApi(data) {}
+function getEquipmentApi(data) { }
