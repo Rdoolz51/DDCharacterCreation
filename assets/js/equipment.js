1ade86b91e51bcd74d23807528ca9342c101e052
Ryan Dooley <Rdoolz51@aol.com>
2022-07-06 18:14:44 -0400

    searches through all API urls and navigates to each endpoint where it chooses which choice to make based off of 'rand' rng

--- assets/js/equipment.js
+++ assets/js/equipment.js
@@ -37,3 +37,79 @@ function queryEquipment(equipmentUrl, choose) {
             return chosen;
         })
 }
+function selectEquipment(from) {
+    const choice = Math.floor(Math.random() * from.length);
+    if (from[choice].equipment) {
+        return Promise.resolve([{
+            name: from[choice].equipment.name,
+            quantity: from[choice].quantity
+        }]);
+    } else if (from[choice]['0']) {
+        let chosen = [];
+        const promises = [];
+
+        if (from[choice]['0'].equipment) {
+            let chosen = [];
+
+            chosen.push({
+                name: from[choice]['0'].equipment.name,
+                quantity: from[choice]['0'].quantity
+            });
+
+            promises.push(Promise.resolve(chosen));
+        } else if (!from[choice]['0'].equipment) {
+            promises.push(queryEquipment(
+                from[choice]['0'].equipment_option.from.equipment_category.url,
+                from[choice]['0'].equipment_option.choose));
+        }
+
+        if (from[choice]['1'].equipment) {
+            let chosen = [];
+
+            chosen.push({
+                name: from[choice]['1'].equipment.name,
+                quantity: from[choice]['1'].quantity
+            });
+
+            promises.push(Promise.resolve(chosen));
+        } else if (!from[choice]['1'].equipment) {
+            promises.push(queryEquipment(
+                from[choice]['1'].equipment_option.from.equipment_category.url,
+                from[choice]['1'].equipment_option.choose));
+        }
+
+        if (from[choice]['2']) {
+            if (from[choice]['2'].equipment) {
+                let chosen = [];
+    
+                chosen.push({
+                    name: from[choice]['2'].equipment.name,
+                    quantity: from[choice]['2'].quantity
+                });
+    
+                promises.push(Promise.resolve(chosen));
+            } else if (!from[choice]['2'].equipment) {
+                promises.push(queryEquipment(
+                    from[choice]['2'].equipment_option.from.equipment_category.url,
+                    from[choice]['2'].equipment_option.choose));
+            }
+        }
+        
+        return Promise.all(promises)
+            .then(values => {
+                return values.flat();
+            });
+    } else {
+        const url = from[choice].equipment_option
+            ? from[choice].equipment_option.from.equipment_category.url
+            : from[choice].equipment_category.url
+
+        const choose = from[choice].equipment_option
+            ? from[choice].equipment_option.choose
+            : 1
+
+        return queryEquipment(
+            url,
+            choose);
+    }
+}
