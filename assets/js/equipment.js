ae17930af5047376110d67a4ae8f818ce67e9b1a
Ryan Dooley <Rdoolz51@aol.com>
2022-07-06 18:13:14 -0400

    added a way to search through the equipment APIs to find certain items

--- assets/js/equipment.js
+++ assets/js/equipment.js
@@ -18,3 +18,22 @@ var CharacterAttributes = {
 
 var charAtt = localStorage.getItem('character');
 CharacterAttributes = JSON.parse(charAtt)
+
+function queryEquipment(equipmentUrl, choose) {
+    return fetch(`https://www.dnd5eapi.co${equipmentUrl}`) //from[choice].equipment_option.from.equipment_category.url
+        .then(function (response) {
+            if (response.ok) {
+                return response.json()
+            }
+        })
+        .then(function (data) {
+            let chosen = [];
+            //from[choice].equipment_option.choose
+            for (let i = 0; i < choose; i++) {
+                const equipChoice = Math.floor(Math.random() * data.equipment.length)
+                chosen.push({ name: data.equipment[equipChoice].name, quantity: 1 })
+
+            }
+            return chosen;
+        })
+}
